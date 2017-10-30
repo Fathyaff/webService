4191e99505c56f42a5fbccd3f72449f814cfe7f9
Fathya Afifah <fathyaff@gmail.com>
2017-10-31 02:23:48 +0700

    register

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         '/ewallet/ping',
+        '/ewallet/register',
         '/ewallet/getSaldo',
         '/ewallet/getTotalSaldo',
         '/ewallet/transfer'
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::post('/ewallet/ping', 'MyController@ping');
-Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => 'csrf'], function () {
     Route::post('/ewallet/register', 'MyController@register');
     Route::post('/ewallet/getSaldo/', 'MyController@getSaldo');
     Route::post('/ewallet/getTotalSaldo', 'MyController@getTotalSaldo');
